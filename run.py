2331dd0d7e2616d035874a9aad7326cc2ca46f4d
Nikolina Weg Krsticevic <nina.weg@gmail.com>
2018-12-02 13:55:57 +0000

    Project setting up

---
+++ run.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template, request, redirect
+import json
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template("index1.html")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   app.run(host=os.environ.get('IP'),
+           port=int(os.environ.get('PORT')),
+           debug=True)
